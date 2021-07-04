984a883b9ab8cfb0b4025f4c044c56876a82c3d9
erdd2218 <github email address>
2021-07-04 10:33:05 -0400

    commit tree 98b9a4fe3b07ba11d3404a5d6a707d42fd0a0ed7

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,18 @@
 def turn_count(board)
+    turn = 0 
+    if board.size == "X" || board.size == "O"
+        
+        turn += 1 
+    end
+    turn
+end 
+
+def current_player(board)
+    turn_count(board) % 2 == 0 ? "X" : "O"
+  end
+
+
+  def turn_count(board)
     # board.count{|token| token == "X" || token == "O"}
   
     turns = 0
